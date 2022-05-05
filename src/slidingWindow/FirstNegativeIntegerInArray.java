e193ba966e7621658b055287f42381b171673047
AroraD <d.deepak.arora@oracle.com>
2022-05-05 22:43:49 +0530

    Added code to find the first negative integer in a subarray

--- src/slidingWindow/FirstNegativeIntegerInArray.java
+++ src/slidingWindow/FirstNegativeIntegerInArray.java
@@ -5,8 +5,9 @@ import java.util.List;
 
 public class FirstNegativeIntegerInArray {
 
+    //We will return 0 if there is no negative integer in the sub array
     public static void main(String[] args) {
-        int[] arr = new int[] {-10,20,-30,-4,5,6};
+        int[] arr = new int[] {-10,20,-30,-4,5,6,8};
         int k = 3;
         int size = arr.length;
 
@@ -21,9 +22,13 @@ public class FirstNegativeIntegerInArray {
                 negativeNumbers.add(arr[j]);
             }
             if(j-i+1 == k) {
-                System.out.println(negativeNumbers.get(0));
-                if (arr[i]==negativeNumbers.get(0)) {
-                    negativeNumbers.remove(0);
+                if(negativeNumbers.size()>0) {
+                    System.out.println(negativeNumbers.get(0));
+                    if (arr[i]==negativeNumbers.get(0)) {
+                        negativeNumbers.remove(0);
+                    }
+                } else {
+                    System.out.println(0);
                 }
                 i++;
             }
