ec4cc9c7ecfc76a68162e4ab1ddbbadd51218ef3
AroraD <d.deepak.arora@oracle.com>
2022-05-30 23:47:56 +0530

    division with bit

--- src/leetcode/DivisionWithBit.java
+++ src/leetcode/DivisionWithBit.java
@@ -3,19 +3,17 @@ package leetcode;
 public class DivisionWithBit {
 
     public static void main(String[] args) {
-        System.out.println(divide(50,5));
+        System.out.println(divide(50,9));
     }
 
     public static int divide(int A, int B) {
-        if (A == Integer.MIN_VALUE && B == -1) {
-            return Integer.MAX_VALUE;
+        if (A == 1 << 31 && B == -1) return (1 << 31) - 1;
+        int a = Math.abs(A), b = Math.abs(B), res = 0, x = 0;
+        while (a - b >= 0) {
+            for (x = 0; a - (b << x << 1) >= 0; x++);
+            res += 1 << x;
+            a -= b << x;
         }
-        int a = Math.abs(A), b = Math.abs(B), res = 0;
-        for (int x = 31; x >= 0; x--)
-            if ((a >>> x) - b >= 0) {
-                res += 1 << x;
-                a -= b << x;
-            }
         return (A > 0) == (B > 0) ? res : -res;
     }
 }
