653ed8405d12e633e1f4313df4e5be5a5652abeb
AroraD <deepakarora0145@gmail.com>
2022-06-13 23:47:04 +0530

    Added Binary Search To find the no

--- src/leetcode/binarySearch/FindElementInSortedRotatedArray.java
+++ src/leetcode/binarySearch/FindElementInSortedRotatedArray.java
@@ -18,8 +18,38 @@ public class FindElementInSortedRotatedArray {
         System.out.println(indexOfTarget);
         indexOfTarget = binarySearch(new int[] {4,6,7,9,1,2,3}, 3, index);
         System.out.println(indexOfTarget);
-        indexOfTarget = binarySearch(new int[] {4,6,7,9,1,2,3}, 10, index);
+        indexOfTarget = search(new int[] {4,6,7,9,1,2,3}, 10);
         System.out.println(indexOfTarget);
+        indexOfTarget = search(new int[] {4,6,7,9,1,2,3}, 4);
+        System.out.println(indexOfTarget);
+        indexOfTarget = search(new int[] {4,6,7,9,1,2,3}, 2);
+        System.out.println(indexOfTarget);
+    }
+
+
+    public static int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end){
+            int mid = (start + end) / 2;
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[start] <= nums[mid]){
+                if (target < nums[mid] && target >= nums[start])
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+
+            if (nums[mid] <= nums[end]){
+                if (target > nums[mid] && target <= nums[end])
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+        }
+        return -1;
     }
 
     private static int binarySearch(int[] arr, int target, int minElement) {
